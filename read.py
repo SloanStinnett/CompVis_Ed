9e9c997ee92f0345eb4819c3a388732819cc6fce
SloanStinnett <stinnettsloan@gmail.com>
2022-06-26 19:56:28 -0500

    import and transform the dolphin image

--- read.py
+++ read.py
@@ -3,5 +3,15 @@ from cv2 import imread
 
 img = imread('Photos/dolphin.jpg')
 
-cv.imshow('Dolphin',img)
+g_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+
+subimg = g_img[300:325,0:300]
+
+print(g_img[250,:])
+
+cv.imshow('grayimage',g_img[250,:])
+cv.waitKey(0)
+
+cv.imshow('subimage',subimg)
 cv.waitKey(0)
